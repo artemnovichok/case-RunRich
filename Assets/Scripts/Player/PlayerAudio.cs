01cfddf5ab3817ebd27edb56c3badf7d36fd350a
artemnovichok <88651599+artemnovichok@users.noreply.github.com>
2024-12-16 02:08:46 +0300

    fix

--- Assets/Scripts/Player/PlayerAudio.cs
+++ Assets/Scripts/Player/PlayerAudio.cs
@@ -3,22 +3,46 @@ using UnityEngine;
 
 namespace Player
 {
-    public class PlayerAudio: MonoBehaviour
+    public class PlayerAudio : MonoBehaviour
     {
         [SerializeField] private AudioClip collectCoins;
         [SerializeField] private AudioClip loseCoins;
         [SerializeField] private AudioClip click;
         [SerializeField] private AudioClip fail;
         [SerializeField] private AudioClip win;
-        
+
+        [SerializeField] private AudioClip[] footsteps;
+        private float timer = 0f;
+
         [NonSerialized] public static PlayerAudio Instance;
         private AudioSource _source;
-        
+
         private void Start()
         {
             Instance = this;
             _source = GetComponentInChildren<AudioSource>();
         }
+
+        private void Update()
+    {
+        if (GameManager.Instance.GameIsOn)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= 0.5f)
+            {
+                timer = 0;
+                int index = UnityEngine.Random.Range(0, footsteps.Length);
+                _source.PlayOneShot(footsteps[index]);
+            }
+        }
+        else
+        {
+            timer = 0;
+        }
+    }
+
+
 
         public void CollectCoins() => _source.PlayOneShot(collectCoins);
         public void LoseCoins() => _source.PlayOneShot(loseCoins);
